b28da212f6c3284922560ff1355b6113b26a81ba
Sindhani <Sindhani@users.noreply.github.com>
2024-12-02 14:08:06 +0000

    Fix styling

--- config/hc-external-providers.php
+++ config/hc-external-providers.php
@@ -3,5 +3,5 @@
 // config for Sindhani/HcExternalProviders
 return [
     'base_url' => env('hc_external_providers_base_url'),
-    'api_token' => env('hc_external_providers_api_token')
+    'api_token' => env('hc_external_providers_api_token'),
 ];
